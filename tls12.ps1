397eb0564a783765a2d63768cb07938245ef4504
Kissima Training <kissima.traning@hotmail.com>
2023-05-03 21:16:08 +0200

    Added a new ps1 file

---
+++ tls12.ps1
@@ -0,0 +1,16 @@
+# Supported tls versions (SystemDefault, Ssl3, Tls, Tls11, Tls12, Tls13)
+[Net.ServicePointManager]::SecurityProtocol
+
+# Set TLS version
+[net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::SystemDefault
+
+[Net.SecurityProtocolType]
+[enum]::GetNames([Net.SecurityProtocolType])
+
+# Enable TLS 1.2
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -Force
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -Force
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'Enabled' -value '1' �PropertyType 'DWORD'
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'DisabledByDefault' -value '0' �PropertyType 'DWORD'
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'Enabled' -value '1' �PropertyType 'DWORD'
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'DisabledByDefault' -value '0' �PropertyType 'DWORD'
